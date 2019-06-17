ada8e52a7f64118da46ced1098f4b78628df0f07
moseslucas <mo@moseslucas.com>
2019-06-17 13:41:21 +0800

    Errors for Game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,14 +8,18 @@ class GamesController < ApplicationController
   def create
     @game = Game.new game_params
     if @game.save
-      render json: @game
+      render json: { payload: @game }, status: 200
+    else
+      render json: { errors: @game.errors }, status: 422
     end
   end
 
   def destroy
     @game = Game.find params[:id]
     if @game.destroy
-      render json: @game
+      render json: { payload: @game }, status: 200
+    else
+      render json: { errors: @game.errors }, status: 422
     end
   end
 
